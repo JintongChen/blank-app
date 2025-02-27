8904fc33d71b155af86d2f35f20fb8b3309da7a7
JintongChen <jintong.chen@atlascopco.com>
2025-02-27 12:00:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("this is atlascopco!!!")
 def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
-file_Path = "‪C:\Users\a00219912\Downloads\Service_kit_transition_to_Enovia (11).csv"
+file_Path = r"‪C:\Users\a00219912\Downloads\Service_kit_transition_to_Enovia (11).csv"
 st.write("data from enovia zone")
 data = load_data(file_Path)
 st.dataframe(data)
